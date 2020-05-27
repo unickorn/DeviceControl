cd2b25a145907337d20911a6dfde93c702b7f03a
BlazeTheDev <iiflamiinblaze123@gmail.com>
2020-05-27 17:11:45 -0400

    Bringing this back to life ig (#3)

--- src/Unickorn/DeviceControl/Main.php
+++ src/Unickorn/DeviceControl/Main.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Unickorn\DeviceControl;
 
 use pocketmine\event\entity\EntityLevelChangeEvent;
@@ -9,42 +11,51 @@ use pocketmine\network\mcpe\protocol\LoginPacket;
 use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\TextFormat;
+use function array_key_exists;
+use function in_array;
 
 class Main extends PluginBase implements Listener{
 
-    private $deviceOS;
+	/** @var array $deviceOS */
+	private $deviceOS = [];
+	/** @var string[] $os */
+	private $os = ["Unknown", "Android", "iOS", "macOS", "FireOS", "GearVR", "HoloLens", "Windows 10", "Windows", "Dedicated", "Orbis", "NX"];
 
-    private $os = ["Unknown", "Android", "iOS", "macOS", "FireOS", "GearVR", "HoloLens", "Windows 10", "Windows", "Dedicated", "Orbis", "NX"];
+	public function onEnable() : void{
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+		$this->saveDefaultConfig();
+	}
 
-    public function onEnable(){
-        $this->getServer()->getPluginManager()->registerEvents($this, $this);
-        if(!is_dir($this->getDataFolder())) @mkdir($this->getDataFolder());
-        $this->saveDefaultConfig();
-    }
+	public function onDataPacketReceive(DataPacketReceiveEvent $event) : void{
+		$packet = $event->getPacket();
+		if($packet instanceof LoginPacket){
+			$this->deviceOS[$packet->username] = $packet->clientData["DeviceOS"];
+		}
+	}
 
-    public function onDataPacketReceive(DataPacketReceiveEvent $e){
-        $packet = $e->getPacket();
-        if($packet instanceof LoginPacket){
-        	$this->deviceOS[$packet->username] = $packet->clientData["DeviceOS"];
-        }
-    }
+	/**
+	 * @param EntityLevelChangeEvent $event
+	 * @throws OSNotFoundException
+	 */
+	public function onLevelChange(EntityLevelChangeEvent $event) : void{
+		$player = $event->getEntity();
+		if(!$player instanceof Player) return;
+		if($player->hasPermission("devicecontrol.bypass")) return;
+		if(in_array($event->getTarget()->getFolderName(), $this->getConfig()->get($os = $this->getOS($player->getName())))){
+			$player->sendMessage(TextFormat::RED . $os . TextFormat::WHITE . " users are not allowed to go to that world.");
+			$event->setCancelled();
+		}
+	}
 
-    public function onLevelChange(EntityLevelChangeEvent $e){
-        $p = $e->getEntity();
-        if(!$p instanceof Player) return;
-        if($p->hasPermission("devicecontrol.bypass")) return;
-        if(in_array($e->getTarget()->getFolderName(), $this->getConfig()->get($os = $this->getOS($p->getName())))){
-            $p->sendMessage(TextFormat::RED . $os . TextFormat::WHITE . " users are not allowed to go to that world.");
-            $e->setCancelled();
-        }
-    }
-
-    /**
-     *  @param string $player
-     *  @return string
-     */
-    public function getOS(string $player) : string{
-        if(!array_key_exists($player, $this->deviceOS)) throw new \InvalidKeyException("Player $player either is not online or has not been given a device OS.");
-        return $this->os[$this->deviceOS[$player]];
-    }
+	/**
+	 * @param string $player
+	 * @return string
+	 * @throws OSNotFoundException
+	 */
+	public function getOS(string $player) : string{
+		if(!array_key_exists($player, $this->deviceOS)){
+			throw new OSNotFoundException("Player $player either is not online or has not been given a device OS.");
+		}
+		return $this->os[$this->deviceOS[$player]];
+	}
 }
---
+++ src/Unickorn/DeviceControl/OSNotFoundException.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Unickorn\DeviceControl;
+
+use Exception;
+
+class OSNotFoundException extends Exception{
+
+}
